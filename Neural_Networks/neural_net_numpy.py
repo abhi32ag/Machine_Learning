1227fcde60cc5c28cf9f20770dee719c14af52e7
Abhinav <garg.abhinav2015@gmail.com>
2017-03-31 11:13:02 -0700

    Updating single layer feed forward network code

--- Neural_Networks/neural_net_numpy.py
+++ Neural_Networks/neural_net_numpy.py
@@ -8,4 +8,12 @@ class NeuralNetwork:
 
 if "__name__" == "__main__":
 
-	
+	neural_network = NeuralNetwork()
+
+	print('Random starting synaptic weights')
+	print(neural_network.synaptic_weights)
+
+	#In the training set we have four examples, each consists of 3 input values 
+	# and 1 output value 
+	training_set_inputs = np.array([0,0,1], [1,1,1], [1,0,1], [0,1,1])
+	training_set_outputs = np.array([])
