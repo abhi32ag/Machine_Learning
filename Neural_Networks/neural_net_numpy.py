f8aac581af3eab3250478bca958562ec84540889
Abhinav <garg.abhinav2015@gmail.com>
2017-03-31 11:28:54 -0700

    Updating single layer feed forward network code

--- Neural_Networks/neural_net_numpy.py
+++ Neural_Networks/neural_net_numpy.py
@@ -2,7 +2,18 @@ import numpy as np
 
 
 
-class NeuralNetwork:
+class NeuralNetwork():
+
+	def __init__(self):
+		# seed the random number generator 
+		np.random.seed(1)
+
+		# We model a single neuron, with 3 input connections and 1 output 
+		# we assign random weights to the 3 x 1 matrix, values in range -1 to 1 
+		# and mean 0 
+		self.synaptic_weights = 2 * np.random.rando((3,1)) - 1
+
+		
 	
 
 
@@ -23,4 +34,9 @@ if "__name__" == "__main__":
 
 	print('New synaptic weights after training ')
 	print(neural_network.synaptic_weights)
-	
+
+	#test the neural network 
+
+	print('Considering new situation [1,0,0]')
+	print(neural_network.predict(np.array([1,0,0])))
+
